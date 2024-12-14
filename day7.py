ea0a1bd712e98d7f7de999d4d20ecf6c9aabaa16
Poccket <ccrystal.cclods@gmail.com>
2024-12-13 21:10:25 -0600

    Day 7 Part 1 & 2

---
+++ day7.py
@@ -0,0 +1,64 @@
+
+from datetime import datetime
+import common
+test_data = common.load_day(7, True)
+now = datetime.now()
+
+def rebase(n, b, pad=0):
+    if n == 0:
+        return '0'.rjust(pad, '0')
+    nums = []
+    while n:
+        n, r = divmod(n, b)
+        nums.append(str(r))
+    return ''.join(reversed(nums)).rjust(pad, '0')
+
+data = []
+for datum in test_data:
+    data += [[datum.split(":")[0], datum.split(": ")[1].split(" ")]]
+
+operators = ("+","*")
+total = 0
+for datum in data:
+    for i in range(2**(len(datum[1])-1)):
+        evald = 0
+        for x, var in enumerate(datum[1]):
+            if x == 0:
+                evald = int(var)
+            else:
+                evald = eval(f"{evald}{operators[(i >> x-1) & 1]}{var}")
+                if evald > int(datum[0]):
+                    break
+        if evald == int(datum[0]):
+            total += evald
+            break
+print(total)
+time = datetime.now() - now
+print(time)
+
+# Part 2
+now = datetime.now()
+operators = ("+","*","||")
+total = 0
+for datum in data:
+    for i in range(3**(len(datum[1])-1)):
+        tern = rebase(i, 3, pad=len(datum[1]))
+        evald = 0
+        for x, var in enumerate(datum[1]):
+            if x == 0:
+                evald = int(var)
+            else:
+                op = operators[int(tern[-x])]
+                if op == "||":
+                    evald = int(str(evald) + var)
+                else:
+                    evald = eval(f"{evald}{op}{var}")
+                    if evald > int(datum[0]):
+                        break
+        if evald == int(datum[0]):
+            total += evald
+            break
+print(total)
+
+time = datetime.now() - now
+print(time)
